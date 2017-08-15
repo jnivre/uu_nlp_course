24fd9d0b22a5993b61876c75a24b64ee45a611b4
miryam <mdelhoneux@gmail.com>
2017-08-15 12:03:26 +0200

    started making assignments into labs

--- code/tokenizer1.py
+++ code/tokenizer1.py
@@ -1,5 +1,6 @@
 import re, sys
 
+regex_pattern = "([,;:.!?\"]|\w+)"
 for line in sys.stdin:
-    for token in re.findall("([,;:.!?\"]|\w+)", line.strip()):
+    for token in re.findall(regex_pattern,line.strip()):
         print(token)
